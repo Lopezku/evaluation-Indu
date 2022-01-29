fe282d28e4d17db6835599c02883dba3ab3ac594
lopez <jerome_lopez@hotmail.Com>
2022-01-29 09:42:32 +0100

    eslint format

--- public/js/functionsToTest.js
+++ public/js/functionsToTest.js
@@ -1,24 +1,28 @@
+/* eslint-disable no-unused-vars */
 const returnAnObject = (...args) => {
   let response = {};
   if (args.length) {
     let index = 0;
-    args.forEach(arg => {
+    args.forEach((arg) => {
       response[index] = arg;
       index++;
-    })
+    });
   } else {
-    response = 'No argument was given to the function.';
-  };
+    response = "No argument was given to the function.";
+  }
   return response;
 };
-
+/* eslint-disable no-unused-vars */
 const multiplyAllByTwo = (arrayOfNumbers) => {
   let response;
-  if (arrayOfNumbers.constructor.prototype === new Array().constructor.prototype) {
-    response = arrayOfNumbers.map((val => val * 2));
-    console.log('arrayTimesTwo: ', arrayTimesTwo);
+  if (
+    /* eslint-disable-next-line */
+    arrayOfNumbers.constructor.prototype === new Array().constructor.prototype
+  ) {
+    response = arrayOfNumbers.map((val) => val * 2);
+    console.log("arrayTimesTwo: ", arrayTimesTwo);
   } else {
-    response = 'The argument is not an Array of numbers';
-  };
+    response = "The argument is not an Array of numbers";
+  }
   return response;
 };
